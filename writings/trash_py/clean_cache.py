b4f1ffb5c1d63e1ced56cdc382c415d5b5422ec1
Luke Miles <lukem@sent.com>
2026-07-25 02:20:06 -0700

    .

--- writings/trash_py/clean_cache.py
+++
@@ -1,8 +0,0 @@
-import json
-cache = json.load(open('url_cache.json'))
-for k, v in cache.items():
-    if v.startswith('[IMAGE DESCRIPTION:'):
-        desc = v[19:-1].strip().replace('\n', ' ').replace('*', '').replace('`', '').replace('#', '')
-        if len(desc) > 600: desc = desc[:597] + '...'
-        cache[k] = f"[IMAGE DESCRIPTION: {desc}]"
-json.dump(cache, open('url_cache.json', 'w'))
