f176442c9bd69b38a0ab8c01ef8ca77382f7bc8f
Luke Miles <lukem@sent.com>
2026-07-25 16:05:15 -0700

    lastdb.c client.mjs

--- writings/lastdb.c
+++ writings/lastdb.c
@@ -2574,7 +2574,7 @@ w2_sum += eff_w * eff_w;
                                 if (max_wl < 0) max_wl = 0;
 double ess = count_est > 0 && w2_sum > 0 ? count_est * count_est / w2_sum : (count_est > 0 ? count_est : 0);
 double confidence = ess >= 1000 ? 1.0 : ess / 1000.0;
-if (max_w < 20.0 && max_w > 0) confidence *= max_w / 20.0;
+if (max_wl > 0) confidence *= (20.0 - max_wl) / 20.0;
                                 if (count_est < 0.5) count_est = 0;
                                 if (__builtin_fabs(sum) < 5e-15) sum = 0;
                                 if (confidence < 0.10) { count_est = 0; sum = 0; confidence = 0; }
@@ -3078,7 +3078,7 @@ if (threshold > 1.0) threshold = 1.0;
                 if (offs) munmap(offs, count * 8);
                 double conf = count_est > 0 ? (double)raw_c / __builtin_sqrt(count_est) : 1.0;
                 if (conf > 1.0) conf = 1.0;
-                if (max_w < 20.0) conf *= max_w / 20.0;
+                if (max_wl > 0) conf *= (20.0 - max_wl) / 20.0;
                 printf("%.4g\t%llu\t%d\t%.4g\n", count_est, (unsigned long long)raw_c, max_wl, conf);
             }
             else if (!strcmp(args[0], "sum") && n >= 2) {
