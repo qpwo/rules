009fbcd904236e63562e4fd15b3593bb71059ca5
Luke Miles <lukem@sent.com>
2026-07-25 14:13:38 -0700

    lastdb.c client.mjs

--- writings/lastdb.c
+++ writings/lastdb.c
@@ -2569,7 +2569,7 @@ if (match) {
                                 } else {
                                     char val[128]; int vl_out = 0;
 if (max_wl < 0) max_wl = 0;
-                                if (max_wl > 0 && raw_count > 0 && raw_count < (1ULL << max_wl)) { if (!has_decay) count_est = (double)raw_count; sum = raw_sum; max_wl = 0; }
+                                if (max_wl > 0 && raw_count > 0 && raw_count < (1ULL << max_wl)) { count_est = (double)raw_count; sum = raw_sum; max_wl = 0; }
                                 if (op == 8) vl_out = snprintf(val, sizeof(val), "%.4g\t%llu\t%d", count_est, (unsigned long long)raw_count, max_wl);
                                 else vl_out = snprintf(val, sizeof(val), "%.17g\t%.17g\t%llu\t%d", sum, raw_sum, (unsigned long long)raw_count, max_wl);
                                     send_response(fd, cipherkey, 0, val, vl_out);
