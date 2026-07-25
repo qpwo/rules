cd21c8f45ddf1706632f436684d99e789c96f617
Luke Miles <lukem@sent.com>
2026-07-25 08:19:35 -0700

    lastdb.c client.mjs

--- writings/lastdb.c
+++ writings/lastdb.c
@@ -128,14 +128,21 @@ static uint64_t key_hash(const char *t, uint16_t tl, const char *k, uint16_t kl)
 }
 
 static uint64_t ht_lower_bound(uint64_t hash) {
-    uint64_t lo = 0;
-    uint64_t hi = ht_sorted_len;
-    while (lo < hi) {
-        uint64_t mid = lo + ((hi - lo) >> 1);
-        if (ht[mid].hash < hash) lo = mid + 1;
-        else hi = mid;
+    uint64_t length = ht_sorted_len;
+    if (length == 0) return 0;
+    uint64_t step = 1ULL << (63 - __builtin_clzll(length));
+    uint64_t begin = 0;
+    if (step != length && ht[step].hash < hash) {
+        length -= step + 1;
+        if (length == 0) return step + 1;
+        step = length <= 1 ? length : 1ULL << (64 - __builtin_clzll(length - 1));
+        begin = ht_sorted_len - step;
     }
-    return lo;
+    for (step >>= 1; step != 0; step >>= 1) {
+        if (ht[begin + step].hash < hash)
+            begin += step;
+    }
+    return begin + (ht[begin].hash < hash);
 }
 
 static void ht_tenant_range(const char *t, uint16_t tl, uint64_t *start_idx, uint64_t *end_idx) {
